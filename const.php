ba7751d49f54feee930c13913097f69e161d70a1
serif <serifcicek92@gmail.com>
2022-05-14 13:05:46 +0300

    const

--- const.php
+++ const.php
@@ -8,8 +8,8 @@ $dbuser="root";
 $dbpassword="";
 $charset="utf8";
 define("THEMANAME","rappo");
-define('SITEADRESS','http://localhost:8888/agsteknik/');
-// define('SITEADRESS','http://agsteknik.com/');
+//define('SITEADRESS','http://localhost:8888/agsteknik/');
+define('SITEADRESS','http://agsteknik.com/');
 // bu kısmı değiştir#
 define('BASEURL', $base_url);
 define('INCLUDEPATH',get_include_path());
